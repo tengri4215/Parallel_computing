cb62662a4bf0107c1d6b11889b71f1b1f0cd29d8
Aliaksei Mironchyk <alexm68007@gmail.com>
2023-11-27 18:34:25 +0300

    add Caller

---
+++ Caller/Caller.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <format>
+
+using namespace std;
+
+const std::initializer_list<int> firstTileSizes = { 1, 2, 5, 10, 20, 50, 100, 200, 500 };
+const std::initializer_list<int> secondTileNumbers = { 1, 2, 5, 10, 20, 50, 100, 200, 500 };
+const std::initializer_list<int> matrixSizes = {  100, 500, 1000, 2000 };
+const std::initializer_list<int> threadNumbers = { 3, 5, 10, 16, 20, 25 };
+
+int main()
+{
+	//write any other calls here
+	system(format("mpiexec -n {} ./pain.exe {} {} {}", 3, 1, 2, 2).c_str());
+	for (int i : firstTileSizes) {
+		for (int j : secondTileNumbers) {
+			for (int n : matrixSizes) {
+				for (int t : threadNumbers) {
+					if (i < n && j < n && t <= ceil(n / i) * j) {
+						system(format("mpiexec -n {} ./pain.exe {} {} {}", t, i, j, n).c_str());
+					}
+				}
+			}
+		}
+	}
+	return 0;
+}
